181f291726374cc781c0108edffce9afb2d94b49
DESKTOP-I4FPN0M\nhath <nhathoangn0@gmail.com>
2022-04-26 07:47:42 +0700

    first commit

---
+++ src/main/java/com/example/wekaspringbootml/WekaSpringBootMlApplication.java
@@ -0,0 +1,13 @@
+package com.example.wekaspringbootml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekaSpringBootMlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WekaSpringBootMlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wekaspringbootml/WekaSpringBootMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wekaspringbootml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekaSpringBootMlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
