92d69bbc3675f9706d5e171547047fa201bcd159
DESKTOP-I4FPN0M\nhath <nhathoangn0@gmail.com>
2022-05-14 22:07:55 +0700

    add Kmeans

--- src/com/company/KMeansModel.java
+++ src/com/company/KMeansModel.java
@@ -1,10 +1,42 @@
 package com.company;
 
+import weka.classifiers.Evaluation;
 import weka.clusterers.SimpleKMeans;
 import weka.core.EuclideanDistance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
 
-public class KMeansModel {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
+public class KMeansModel extends MyDataModel {
+
+    SimpleKMeans simpleKMeans;
+    Evaluation evaluation;
+    public KMeansModel(){}
+    public KMeansModel(String filename, String model_option, String data_option) throws Exception{
+        super(filename,model_option,data_option);
+    }
+    public void buildKMeansModel(String fileName) throws Exception{
+        setTrainSet(fileName);
+        simpleKMeans = new SimpleKMeans();
+        simpleKMeans.setNumClusters(3);
+        simpleKMeans.setDistanceFunction(new EuclideanDistance());
+        simpleKMeans.buildClusterer(trainSet);
+        System.out.println(simpleKMeans);
+    }
+    public void predictLabel(String fileName, String fileOut) throws Exception {
+        ConverterUtils.DataSource dataSource1 = new ConverterUtils.DataSource(fileName);
+        Instances instances = dataSource1.getDataSet();
+        for(int i=0; i<instances.numInstances(); i++)
+        {
+            double pre = simpleKMeans.clusterInstance(instances.instance(i));
+            instances.instance(i).setClassValue(pre);
+        }
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileOut));
+        bufferedWriter.write(instances.toString());
+        bufferedWriter.newLine();;
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-//        KNNModel model = new KNNModel("", null ,null);
-//        model.buildKNN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\car_bin.arff");
-//        model.evaluationKNN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\car_bin_test.arff");
-//        model.predictLabel("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\unlabel.arff", "C:\\Users\\nhath\\Documents\\pre.arff");
-//        System.out.println(model);
-        ANNModel model = new ANNModel("",
-                "-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a -R"
-                ,null);
-        model.buildANNModel("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\90.arff");
-        model.evaluationANN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\10.arff");
+        KNNModel model = new KNNModel("", null ,null);
+        model.buildKNN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\70.arff");
+        model.evaluationKNN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\30.arff");
         model.predictLabel("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\unlabel1.arff", "C:\\Users\\nhath\\Documents\\pre.arff");
         System.out.println(model);
+//        ANNModel model = new ANNModel("",
+//                "-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a -R"
+//                ,null);
+//        model.buildANNModel("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\70.arff");
+//        model.evaluationANN("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\30.arff");
+//        model.predictLabel("E:\\Learning\\Java\\Machine_Learning_Spring_Weka\\src\\main\\resources\\arffs\\unlabel1.arff", "C:\\Users\\nhath\\Documents\\pre.arff");
+//        System.out.println(model);
 
     }
 }
